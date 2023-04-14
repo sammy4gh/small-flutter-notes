d9b0c0cd24d8a3fcc782c414ee5fe8ffd202e620
sammy <aristocrat4gh@gmail.com>
2023-04-14 13:33:56 +0000

    preparing to create new_notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/view/login_view.dart';
-import 'package:mynotes/view/notes_view.dart';
+import 'package:mynotes/view/notes/new_note_view.dart';
+import 'package:mynotes/view/notes/notes_view.dart';
 import 'package:mynotes/view/register_view.dart';
 import 'package:mynotes/view/verify_email_view.dart';
-
-import 'firebase_options.dart';
 
 main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +17,8 @@ main() {
       loginRoute: (context) => const LoginView(),
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
-      verifyEmaiLRoute: (context) => const VerifyEmailView()
+      verifyEmaiLRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
